77b7343d0f85edf1bbf8c1435ef83845c315507b
Maxence Choiselle <mchoiselle@gmail.com>
2025-02-12 10:38:36 +0100

    Feat ajout de la base du site

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <nav className="nav">
+          <div className="logo">
+            <h1>Portfolio</h1>
+          </div>
+          <ul className="nav-links">
+            <li><a href="#about">À propos</a></li>
+            <li><a href="#skills">Compétences</a></li>
+            <li><a href="#projects">Projets</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
